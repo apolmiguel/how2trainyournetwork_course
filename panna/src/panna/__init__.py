b267e53975663b5be989202ddd453979a6f007e8
Apolinario Miguel Tan <apolmiguel@gmail.com>
2026-01-18 15:38:32 +0100

    cleanup

--- panna/src/panna/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import gvector, lib, neuralnet, jax, interfaces
-from .lib.log import init_logging
-
-__all__ = ['gvector', 'lib', 'neuralnet', 'jax', 'interfaces']
-
-init_logging()
--- panna/src/panna/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from .examplejsonwrapper import ExampleJsonWrapper
-from .log import init_logging
-from .tfr_data_structure import tfr_writer, example_tf_packer
-
-__all__ = ['init_logging', 'ExampleJsonWrapper', 'tfr_writer', 'example_tf_packer']
